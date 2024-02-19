f0bef52affb79fe6baf0e55a86c407eb4a9f17f7
Tiorna SORO <tiornasoro094@gmail.com>
2024-02-19 10:48:01 +0100

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import pickle
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
@@ -45,3 +46,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
